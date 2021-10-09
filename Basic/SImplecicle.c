dd503add954eaf502e647734b5af07c3120b3ba7
MárioPinto01 <mario.joao31@hotmail.com>
2021-10-09 23:49:20 +0100

    ciclo simples

---
+++ Basic/SImplecicle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int bola = 0;
+    int i=0;
+    for( i= 0;i <=2 ;i= i + 1){
+       
+        printf("Relembrar coisas que dei no 1%i ano!\n",bola);
+         bola++;
+    };
+    return 0;
+}
