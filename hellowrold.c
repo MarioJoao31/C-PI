278a89471100d1a8b4c52a79c6c90b628478977b
MárioPinto01 <mario.joao31@hotmail.com>
2021-10-10 23:48:26 +0100

    bumb secondary functions

--- hellowrold.c
+++ hellowrold.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello World");
+int main( int argc, char* argv[]){
+
+    printf("Idiota: %i\n", argc);
+
+    if (argc > 1){
+        printf("Primeiro argumento: %s\n", argv[1]);
+    };
+
+    char second();
+    char tone(second);
+
+    
+    
     return 0;
+}
+char tone(second){
+
+        
+        return(printf("Nome=%d",second));
+    };
+
+char second(){
+    
+    return("MArio");
 }
