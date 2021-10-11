f1cec13cdc8003548d83bfff6e94d7f4e82674a0
MárioPinto01 <mario.joao31@hotmail.com>
2021-10-11 11:43:18 +0100

    Test2

--- Basic/SImplecicle.c
+++ Basic/SImplecicle.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int bola = 0;
-    int i=0;
-    for( i= 0;i <=2 ;i= i + 1){
-       
-        printf("Relembrar coisas que dei no 1%i ano!\n",bola);
-         bola++;
+    int i = 0;
+    char x[4] = "CONAPUTA";
+    for (i = 0; i <= 2; i = i + 1)
+    {
+        printf("%s", x);
+        printf("Relembrar coisas que dei no 1%i ano!\n", bola);
+        bola++;
     };
     return 0;
 }
