052fe379aafc8ba6f892ca5ab4ca8e32d1c99256
MárioPinto01 <mario.joao31@hotmail.com>
2021-10-13 11:37:13 +0100

    media 10 alunos

--- Basic/Media-10alunos.c
+++ Basic/Media-10alunos.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    int i,nota,notaincre = 0;
     
+    
+    while(i <=9){
+        printf("introduza o nota do aluno:");
+        scanf("%d",&notaincre);
+        nota=nota+notaincre;
+        i++;
+    };
+
+    nota=nota/10;
+    printf("media:%d",nota);
+
 }
